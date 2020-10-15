95f687fccd522156d5f48365c7d37a9a09d40c22
PONCEJE <PONCEJE@OOCL.COM>
2020-10-15 20:12:56 +0800

    [MarsRover] [Jeyem] Test: should return x=-1 y=1 heading=N when execute given x=0 y=0 heading=N and command=MLMR

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -3,6 +3,7 @@ package com.afs.tdd;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DemoTest {
     @Test
@@ -137,4 +138,16 @@ class DemoTest {
         assertEquals(0, marsRover.getLocationY());
         assertEquals("N", marsRover.getHeading());
     }
+    @Test
+    void should_return_x_nega1_y_1_heading_N_when_execute_given_x_0_y_0_heading_N_and_command_MLMR() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"N");
+        //when
+        marsRover.executeCommands("MLMR");
+        //then
+        assertEquals(-1, marsRover.getLocationX());
+        assertEquals(1, marsRover.getLocationY());
+        assertEquals("N", marsRover.getHeading());
+    }
+
 }
