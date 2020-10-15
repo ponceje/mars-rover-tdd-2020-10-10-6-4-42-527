f87b6c8cc891b794722e717e4fa9bb13ac883253
PONCEJE <PONCEJE@OOCL.COM>
2020-10-15 19:09:07 +0800

    [MarsRover] [Jeyem] Test: should return x=0 y=0 heading=E when execute given x=0 y=0 heading=S and command=L

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -60,4 +60,15 @@ class DemoTest {
         assertEquals(0, marsRover.getLocationY());
         assertEquals("W", marsRover.getHeading());
     }
+    @Test
+    void should_return_x_0_y_0_heading_E_when_execute_given_x_0_y_0_heading_S_and_command_L() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"S");
+        //when
+        marsRover.executeCommands("L");
+        //then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("E", marsRover.getHeading());
+    }
 }
