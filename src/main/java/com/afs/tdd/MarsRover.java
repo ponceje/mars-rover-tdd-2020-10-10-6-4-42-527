b5fda4beead0c22818b0f43ce849e06b53014b76
PONCEJE <PONCEJE@OOCL.COM>
2020-10-15 17:02:03 +0800

    [MarsRover] [Jeyem] Test: should return x=0 y=1 heading=N when execute given x=0 y=0 heading=N and command=M

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,21 @@
+package com.afs.tdd;
+
+public class MarsRover {
+    public MarsRover(int i, int i1, String n) {
+    }
+
+    public void executeCommands(String m) {
+    }
+
+    public int getLocationX() {
+        return 0;
+    }
+
+    public int getLocationY() {
+        return 0;
+    }
+
+    public String getHeading() {
+        return null;
+    }
+}
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,18 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
-
+    void should_return_x_0_y_1_heading_N_when_execute_given_x_0_y_0_heading_N_and_command_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"N");
+        //when
+        marsRover.executeCommands("M");
+        //then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(1, marsRover.getLocationY());
+        assertEquals("N", marsRover.getHeading());
     }
 }
