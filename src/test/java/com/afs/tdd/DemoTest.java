780a46f6444388a9d7ee2d9a70769d1a567820fd
PONCEJE <PONCEJE@OOCL.COM>
2020-10-15 20:15:07 +0800

    [MarsRover] [Jeyem] Test: should return CommandNotDefinedException when execute given x=0 y=0 heading=N and command=F

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -149,5 +149,16 @@ class DemoTest {
         assertEquals(1, marsRover.getLocationY());
         assertEquals("N", marsRover.getHeading());
     }
+    @Test
+    void should_return_CommandNotDefinedException_when_execute_given_x_0_y_0_heading_N_and_command_F() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"N");
+        //when
+
+        //then
+        assertThrows(CommandNotDefinedException.class, () -> {
+            marsRover.executeCommands("F");
+        });
+    }
 
 }
